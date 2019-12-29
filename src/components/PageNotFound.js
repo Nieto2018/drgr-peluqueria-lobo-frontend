459b61a64d6b6e7eef33517ca8d5d4d2f76d7d42
Antonio Nieto <nietodjsombra@gmail.com>
2019-12-29 02:39:27 +0100

    PageNotFound page added

---
+++ src/components/PageNotFound.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { translate } from 'react-multi-lang'
+import { withRouter } from 'react-router'
+import toast from './utils/Toast'
+
+import banner from '../styles/images/banner.mp4'
+
+
+function PageNotFound(props) {
+
+    return (
+      <div>
+
+        <section id="main" className="wrapper">
+          <div className="inner">
+            <div className="content">
+              {/* TODO */}
+              Error 404: Page not found
+            </div>
+          </div>
+        </section>
+
+      </div>
+    )
+
+}
+
+export default translate(withRouter(PageNotFound))
