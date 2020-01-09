f56e78e66812594bd2227bde548d6b67fead63b4
Antonio Nieto <nietodjsombra@gmail.com>
2020-01-09 14:20:04 +0100

    Toast enhanced
    Now it show notifications by type

--- src/Constants.js
+++ src/Constants.js
@@ -1,8 +1,19 @@
-// Server constants
-export const BACKEND_SERVER_DOMAIN ='localhost:8000'
-export const BACKEND_SERVER_URL ='http://' + BACKEND_SERVER_DOMAIN
+/*
+Config constants
+*/
+export const TOAST_TYPE = {
+    SUCCESS: 'success',
+    WARNING: 'warn',
+    ERROR: 'error'
+}
+
+/*
+Server constants
+*/
+export const BACKEND_SERVER_DOMAIN = 'localhost:8000'
+export const BACKEND_SERVER_URL = 'http://' + BACKEND_SERVER_DOMAIN
 export const GRAPHQL_URL = BACKEND_SERVER_URL + '/graphql/'
-export const BACKEND_SERVER_WS ='ws://' + BACKEND_SERVER_DOMAIN
+export const BACKEND_SERVER_WS = 'ws://' + BACKEND_SERVER_DOMAIN
 export const GRAPHQL_SUBSCRIPTION_WS = BACKEND_SERVER_WS + '/subscriptions/'
 export const RESET_PASSWORD_URL = BACKEND_SERVER_URL + '/rest-auth/password/reset/'
 
@@ -21,9 +32,9 @@ export const RESET_PASSWORD_CONFIRM_URL = '/account/reset-password-confirm/'
 /*
 Account constants
 */
-export const G_USER_ID ='name'
-export const G_USER_EMAIL ='email'
-export const G_AUTH_TOKEN ='token'
+export const G_USER_ID = 'name'
+export const G_USER_EMAIL = 'email'
+export const G_AUTH_TOKEN = 'token'
 export const ITEMS_PER_PAGE = 1 // setting this only to one so you can easily test your pagination implementation
 
-export const APP_LANGUAGE ='app-language'
+export const APP_LANGUAGE = 'app-language'
--- src/components/utils/Toast.js
+++ src/components/utils/Toast.js
@@ -3,6 +3,20 @@ import 'react-toastify/dist/ReactToastify.css';
 import { toast } from 'react-toastify';
 toast.configure()
 
-const notify = (msg) => toast(msg);
+const notify = (msg, type) => {
+    switch(type){
+        case 'success':
+            toast.success(msg)
+            break
+        case 'warn':
+            toast.warn(msg)
+            break
+        case 'error':
+            toast.error(msg)
+            break
+        default:
+            toast(msg);
+    }
+}
 
 export default notify
